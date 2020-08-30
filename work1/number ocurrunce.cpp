f49504fcc13591f822738090f6d73177b7ff3d91
THANAPAT SITTIPORNCHAISAKUL <oatkung.ts@gmail.com>
2020-08-30 23:10:50 +0700

    Number

---
+++ work1/number ocurrunce.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main() {
+	long int a[250000], n, g;
+	long int i = 0;
+	scanf("%ld", &n);
+	if (n > 1 && n <= 2500000) {
+		for (i = 0; i < n; i++) {
+			scanf("%ld", &a[i]);
+		}
+		if (a[i] > 0 && a[i] < 1000) {
+			scanf("%ld", &g);
+			for (i = 0; i < n; i++) {
+				if (a[i] == g) {
+					printf("%ld ", i + 1);
+				}
+			}
+		}
+		else
+			printf("Error");
+
+	}
+
+	else {
+		printf("Error");
+	}
+}
